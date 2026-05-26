4aedc49a5818364efd93aadfaf12ff49dd7b29c6
GIOVANA BARBOSA SOUZA <25133664@cfp127aluno.local>
2026-05-26 15:56:48 -0300

    Fazendo a model dos doces

---
+++ model/DAO/doce.js
@@ -0,0 +1,148 @@
+/****************************************************************************************
+ * Objetivo: Arquivo responsável pela realização do CRUD no Banco de Dados MySQL
+ * Data: 26/05/2026
+ * Autor: Giovana
+ * Versão: 1.0
+ ****************************************************************************************/
+
+
+//Import da biblioteca do PrismaClient
+const {PrismaClient} = require('../../generated/prisma')
+
+//Cria um objeto do prisma client para manipular os scripts sql
+const prisma = new PrismaClient()
+
+//Retorna todos os doces do banco de dados 
+const getSelectAllCandys = async function () {
+    try{
+        //Script sql
+        let sql = `select * from tbl_doce order by desc`
+
+        //Executa no bc o script sql
+        let result = await prima.$queryRawUnsafe(sql)
+
+        if(Array.isArray(result)){
+            return result
+        } else {
+            return false 
+        }
+    } catch (error) {
+        return false 
+    }
+}
+
+const getSelectByIdCandy = async function (id) {
+
+    try {
+
+        //Script sql
+        let sql = `select * from tbl_doce where id= ${id}`
+
+        //Executa no BD o script SQL
+        let result = await prisma.$executeRawUnsafe(sql)
+
+         //Validação para verificar se o retorno do banco é um ARRAY (vazio ou com dados)
+        if (Array.isArray(result)) {
+            return result
+        } else {
+            return false
+        }
+    } catch (error) {
+        return false
+    }
+    
+}
+
+const setInsertCandys = async function (doce) {
+    try{
+
+        let sql = `INSERT INTO tbl_doce ( tipo, 
+        nome, 
+        massa, 
+        recheio, 
+        cobertura, 
+        qtd, 
+        peso, 
+        porcao, 
+        data_validade, 
+        status)
+        VALUES (
+        '${doce.tipo}',
+        '${doce.nome}',
+        '${doce.massa}'
+        '${doce.recheio}'
+        '${doce.cobertura}'
+        '${doce.qtd}'
+        '${doce.peso}'
+        '${doce.porcao}'
+        '${doce.data_validade}'
+        '${doce.status}');`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else 
+        return false 
+    } catch (error) {
+        return false
+    }
+}
+
+const setUpdateCandys = async function (doce) {
+
+    try{
+
+        let sql = `update tbl_doce set
+        tipo,               =   '${doce.tipo}',
+        nome                =   '${doce.nome}',
+        massa               =   '${doce.massa}'
+        recheio             =   '${doce.recheio}'
+        cobertura           =   '${doce.cobertura}'
+        qtd                 =   '${doce.qtd}'
+        peso                =   '${doce.peso}'
+        porcao              =   '${doce.porcao}'
+        data_validade       =   '${doce.data_validade}'
+        status              =   '${doce.status}'
+        where id_doce = ${doce.id_doce}`
+
+        
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+        return false
+        
+} catch (error) {
+    return false
+}
+
+}
+
+const setDeleteCandys = async function (id) {
+    
+    try {
+        let sql = `delete from tbl_doce where id_doce = ${id}`
+
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result > 0)
+            return true
+        else 
+        return false 
+
+    } catch (error){
+        return false
+    }
+    
+}
+
+module.exports = {
+    getSelectAllCandys,
+    getSelectByIdCandy,
+    setInsertCandys,
+    setUpdateCandys,
+    setDeleteCandys
+}
---
+++ tabelas_doceria.sql
@@ -0,0 +1,40 @@
+Create database db_doceria;
+use db_doceria;
+
+create table tbl_doce (
+id_doce int not null primary key auto_increment,
+tipo varchar(120) not null,
+nome varchar (120)not null,
+massa varchar(120) not null,
+recheio varchar (120) not null,
+cobertura varchar(120) not null,
+qtd int not null,
+peso decimal(10,2) not null,
+porcao int not null,
+data_validade date not null,
+status enum ('Disponivel', 'Descartado') not null
+);
+
+alter table tbl_doce 
+modify column cobertura varchar(120) not null;
+
+
+create table tbl_funcionario  (
+id_funcionario int not null primary key auto_increment,
+nome varchar(120) not null,
+email varchar (120) not null,
+senha varchar (100) not null
+);
+
+create table tbl_historico_descarte (
+id_historico_descarte int not null primary key auto_increment,
+id_doce int not null,
+id_funcionario int not null,
+data_descarte datetime not null default (current_date())
+);
+
+alter table tbl_historico_descarte
+add constraint fk_funcionario_descarte
+foreign key (id_funcionario) references tbl_funcionario (id_funcionario),
+add constraint fk_doce_descarte
+foreign key (id_doce) references tbl_doce (id_doce);
